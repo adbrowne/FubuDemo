552816bef29083ebae89bb00824e85f985e62e39
Andrew Browne <brownie@brownie.com.au>
2012-02-26 16:25:03 +1100

    Fixed name of ContainerRegistry

---
+++ FubuDemo/Infrastructure/ContainerRegistry.cs
@@ -0,0 +1,16 @@
+﻿using FubuDemo.Service;
+using StructureMap;
+using StructureMap.Configuration.DSL;
+using StructureMap.Pipeline;
+
+namespace FubuDemo.Infrastructure
+{
+    public class ContainerRegistry : Registry
+    {
+        public ContainerRegistry()
+        {
+            For<IMovieService>().LifecycleIs(Lifecycles.GetLifecycle(InstanceScope.Singleton))
+                .Use<MovieService>();
+        }
+    }
+}
